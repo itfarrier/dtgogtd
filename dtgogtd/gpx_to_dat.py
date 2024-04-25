68fee2af62a458cef299ae45ad88a00b9443a5cf
Dzmitry Padabed <itfarrier@icloud.com>
2024-04-26 00:40:25 +0300

    add initial gpx_to_dat.py
    Signed-off-by: Dzmitry Padabed <itfarrier@icloud.com>

--- dtgogtd/gpx_to_dat.py
+++ dtgogtd/gpx_to_dat.py
@@ -1,3 +1,8 @@
+from gpx_converter import Converter
+
+
 def gpx_to_dat(input_file, output_file):
+    df = Converter(input_file).gpx_to_dataframe()
+    print("df", df)
     print("input_file", input_file)
     print("output_file", output_file)
